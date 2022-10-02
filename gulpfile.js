3e7c47eda0bd8954f779385620e6a0015cac8206
Donnyves Laroque <40510674+laroqued@users.noreply.github.com>
2022-10-02 16:41:26 -0700

    finished nesting

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var { src, dest, watch } = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
 sass.compiler = require("node-sass");
 function css() {
-  return src("src/*.scss").pipe(sass()).pipe(dest("dist/css"));
+  return src("src/*.scss")
+  .pipe(sass())
+  .pipe(dest("dist/css"));
 }
 
 exports.default = function () {
